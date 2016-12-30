fbb951fcfce468773804edf2e737cb23a269520b
Rob Felsburg <rob@immuta.com>
2016-12-30 10:59:52 -0500

    Enable snmpd, disable snmp client

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class snmp::params {
-    $snmpd_package_ensure             = 'enabled'
+    $snmpd_package_ensure             = 'present'
     $snmpd_package_name               = 'net-snmp'
     $snmpd_package_latest             = false,
     $snmpd_service_name               = 'snmpd'
 
-    $snmp_package_ensure              = 'enabled'
+    $snmp_package_ensure              = 'absent'
     $snmp_package_latest              = false,
     $snmp_package_name                = 'net-snmp-utils'
 }
