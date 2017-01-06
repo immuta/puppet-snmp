dfd6010603833f6c84ccefe44f739094497eb9ec
Rob Felsburg <rob@immuta.com>
2017-01-06 10:45:23 -0500

    dont replace the entire file, just append

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,6 +16,7 @@ class snmp::config {
     group          => 'root',
     mode           => '0700',
     ensure_newline => true,
+    replace        => false,
   }
 
   concat::fragment { 'snmp_v3user':
