6005cc3f9ceb5e5aeadff1494aff6449a70edaf5
Rob Felsburg <rob@immuta.com>
2017-01-09 10:14:29 -0500

    It's not supposed to be astrin you loony linter. And robots don't say ye.

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,7 +10,7 @@ class snmp::service {
     fail('snmpd_package_ensure parameter must be either running, or stopped')
   }
 
-  if ! ($::snmp::snmpd_service_enable in [true, false, 'manual', 'mask']) {
+  if ! ($::snmp::snmpd_service_enable in ['true', 'false', 'manual', 'mask']) {
     fail('snmpd_package_ensure parameter must be either true, false, manual, or mask')
   }
 
