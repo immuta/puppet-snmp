f3e41be6d23c7fd30c1bf866e1d12f9e534066ff
Rob Felsburg <rob@immuta.com>
2017-01-11 15:53:23 -0500

    not checking a variable value, but the variable types itself

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,7 +10,7 @@ class snmp::service {
     fail('snmpd_package_ensure parameter must be either running, or stopped')
   }
 
-  if ! (($::snmp::snmpd_service_enable in ['true', 'false', 'manual', 'mask']) or ($::snmp::snmpd_service_enable.is_bool()) {
+  if ! (($::snmp::snmpd_service_enable in ['true', 'false', 'manual', 'mask']) or ($::snmp::snmpd_service_enable.is_a(Boolean)) {
     fail('snmpd_package_ensure parameter must be either true, false, manual, or mask')
   }
 
